9047d9c4def54ec119e976271004f3e8d27e890b
akshatdeep <akshatdeepastik9@gmail.com>
2024-04-25 15:53:16 +0530

    commmit2

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,22 @@ var express = require("express");
 var router = express.Router();
 const books = require("../models/book");
 const upload = require("../utils/multer").single("image");
+const fs = require("fs");
+const path = require("path");
+
+
+
+
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index");
 });
+
+
+
+
+
 
 router.get("/library", async function (req, res, next) {
   try {
@@ -17,41 +28,78 @@ router.get("/library", async function (req, res, next) {
   }
 });
 
+
+
+
+
 router.get("/create", function (req, res, next) {
   res.render("create");
 });
 
-router.post("/create", upload, async function (req, res, next) {
 
+
+
+
+
+router.post("/create", async (req, res, next) => {
   try {
-    const newbook = new books({...req.body, image:req.file.filename})
+    upload(req, res, async function (error) {
+      if (error) {
+        res.send(error);
+        return;
+      }
+      const newbook = new books({ ...req.body, image:req.file.filename});
+      await newbook.save();
+      res.redirect("/library");
+    });
+    
     // res.json({ body: req.body, file: req.file });
-    await newbook.save()
-    res.redirect("/library",)
   } catch (error) {
     res.send(error);
   }
 });
 
+
+
+
+
 router.get("/delete/:id", async function (req, res, next) {
   try {
-    await books.findByIdAndDelete(req.params.id);
+    const book = await books.findByIdAndDelete(req.params.id);
+    fs.unlinkSync(path.join(__dirname, "..", "public", "images", book.image));
     res.redirect("/library");
   } catch (error) {
     res.send(error);
   }
 });
 
+
+
+
+
 router.get("/about", function (req, res, next) {
   res.render("about");
 });
 
+
+
+
+
+
 router.get("/update/:id", async function (req, res, next) {
   try {
-    const updateBook = await books.findById(req.params.id);
+    const updateBook = await boo;
+    ks.findById(req.params.id);
     res.render("update", { books: updateBook });
-  } catch (error) {}
+  } catch (error) {
+    res.send(error);
+  }
 });
+
+
+
+
+
 
 router.post("/update/:id", async function (req, res, next) {
   try {
@@ -61,5 +109,9 @@ router.post("/update/:id", async function (req, res, next) {
     res.send(error);
   }
 });
+
+
+
+
 
 module.exports = router;
--- utils/multer.js
+++ utils/multer.js
@@ -1,17 +1,32 @@
-const multer = require("multer")
-const path = require('path')
-
+const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/images')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now()+ path.extname(file.originalname))
-    }
-  })
-  
-  const upload = multer({ storage: storage })
+  destination: function (req, file, cb) {
+    cb(null, "./public/images");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
 
+const filefilter = (req, file, cb) => {
+  let allowedFiles = /png|jpg|jpeg|webp|gif/;
+  let minetype = allowedFiles.test(file.mimetype);
+  let extname = allowedFiles.test(
+    path.extname(file.originalname).toLowerCase()
+  );
 
-  module.exports = upload
+  if (minetype && extname) {
+    cb(null, true);
+  } else {
+    cb(`ERROR: ONLY ${allowedFiles} IMAGE EXTENSIONS ARE ALLOWED`, false);
+  }
+};
+
+const upload = multer({ storage: storage, fileFilter: filefilter});
+
+module.exports = upload;
